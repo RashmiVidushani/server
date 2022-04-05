ccdd30c226e9bc46591e61ea807b3eb6897e94d6
Rashmi Vidushani <audrianblackrose@gmail.com>
2022-04-05 14:47:08 -0700

    sds

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const app=express();
 const port = process.env.port||5000;
 var server =http.createServer(app);
 var io= require("socket.io")(server);
+
+
 app.use(express.json());
 var clients={}
+
 io.on("connection",(socket)=>{
     console.log("connected");
     console.log(socket.id,"has joined");
@@ -16,8 +19,8 @@ io.on("connection",(socket)=>{
     });
     socket.on("message",(msg)=>{
         console.log(msg);
-        let tartgetId=msg.tartgetId;
-        if(clients[tartgetId])clients[tartgetId].emit("message",msg);
+        let targetId=msg.targetId;
+        if(clients[targetId]) clients[targetId].emit("message",msg);
     });
 });
 server.listen(port,"0.0.0.0",()=>{
