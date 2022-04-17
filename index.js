b45402bf36ff9cc8c5a3068169a5fceec78214d8
Rashmi Vidushani <audrianblackrose@gmail.com>
2022-04-17 17:57:35 +0530

    dsfvdf

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express=require("express");
 var http=require("http");
+const mysql = require('mysql');
 const app=express();
+const cors = require('cors');
 const port = process.env.port||5000;
 var server =http.createServer(app);
 var io= require("socket.io")(server);
-
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "chatapp",
+});
 
 app.use(express.json());
 var clients={}
@@ -13,6 +20,7 @@ const routes=require("./routes");
 app.use("/routes", routes);
 app.use("/uploads",express.static("uploads"));
 
+
 io.on("connection",(socket)=>{
     console.log("connected");
     console.log(socket.id,"has joined");
@@ -20,6 +28,8 @@ io.on("connection",(socket)=>{
         console.log(id);
         clients[id]=socket;
         console.log(clients);
+        //socket.id==uid in sql server
+        //database.table('signup').withFields(["id","username","bio"])
     });
     socket.on("message",(msg)=>{
         console.log(msg);
@@ -27,6 +37,55 @@ io.on("connection",(socket)=>{
         if(clients[targetId]) clients[targetId].emit("message",msg);
     });
 });
+
+db.connect((err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log('MySql Connected');
+});
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST","DELETE", "PUT"],
+    credentials: true,
+}));
+app.get('/api/getusers', (req, res) => {
+    db.query("SELECT * FROM signup", (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+app.post('/api/setusers', (req, res) => {
+    db.query("Insert INTO signup values (idsignup,signupusername,signupbio)", (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+/*
+app.put('/api/putusers', (req, res) => {
+    db.query("Update signup set idsignup=socket.id,signupusername,signupbio)", (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+app.delete('/api/setusers', (req, res) => {
+    db.query("Insert INTO signup values (idsignup,signupusername,signupbio)", (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});*/
 /*
 app.route("/check").get((req,res)=>{
     return res.json("your app is working fine")
